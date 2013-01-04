f1f888f4dbb167d89c46db0cbcb0d145885b5867
manish <manish@manish-Studio-1555.(none)>
2013-01-04 11:43:12 +0530

    first file

---
+++ ABA12A.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int c;
+    
+    scanf("%d",&c);
+    
+    while(c--)
+    {
+              long long int A,B;
+              
+              cin>>A>>B;
+              
+              cout<<B<<endl;
+    }
+    
+    system ("pause");
+    
+    return 0;
+}
